fdde1cc7e2db6fbbd5e149c9776806b6a83f0221
Foxy Ice <elizaveto4ka8259@gmail.com>
2026-08-17 15:34:45 +0300

    Sixth lesson

---
+++ main.py
@@ -0,0 +1,7 @@
+def show_question(word, translation):
+    print("Слово:", word)
+    print("Правильний переклад:", translation)
+
+show_question("apple", "яблуко")
+show_question("dog", "собака")
+show_question("sun", "сонце")
