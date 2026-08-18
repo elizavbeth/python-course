53340caecd08e8e6caf92854ed979e83d3337500
Foxy Ice <elizaveto4ka8259@gmail.com>
2026-08-18 11:07:14 +0300

    Seventh lesson

--- functions.py
+++ functions.py
@@ -5,9 +5,31 @@ def show_rules():
 
 show_rules()
 
+print("--------параметри / аргументи--------")
+
 def greet(name):
     print("Привіт,", name)
 
 greet("Anna")
 # Параметр — змінна, яку функція очікує отримати (name).
 # Аргумент — конкретне значення, яке ми передаємо під час виклику ("Anna").
+
+print("--------def + return--------")
+
+def calculate_points(answer, correct_answer):
+    if answer == correct_answer:
+        return 10
+    else:
+        return 0
+    
+score = 0
+
+answer = input("Переклади apple: ")
+points = calculate_points(answer, "яблуко")
+score = score + points
+print(score)
+
+answer = input("Переклади dog: ")
+points = calculate_points(answer, "собака")
+score = score + points
+print(score)
--- main.py
+++
@@ -1,7 +0,0 @@
-def show_question(word, translation):
-    print("Слово:", word)
-    print("Правильний переклад:", translation)
-
-show_question("apple", "яблуко")
-show_question("dog", "собака")
-show_question("sun", "сонце")
